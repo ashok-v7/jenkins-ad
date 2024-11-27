e8ca1a30af41b2aa7fa6a2b18897e2a86d2eb56c
Ashok <45220490+ashok-v7@users.noreply.github.com>
2024-11-27 23:27:41 +0530

    Update f3.py

--- f3.py
+++ f3.py
@@ -4,6 +4,7 @@ def read_file_content():
     # Construct the file path relative to the current working directory
     print("getcwd",os.getcwd())
     file_path = os.path.join(os.getcwd(), "data/workspaces/ssops/BTKN")
+    print("file_path-->",file_path)
 
     try:
         # Open the file and read its contents
